afe1150978112ee153d9f27e4a7a005a60412b0b
Dov Sturm <dysturm@gmail.com>
2019-07-02 13:20:39 -0400

    init commit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Recruiters()
+        {
+            ViewBag.Message = "Add New Recruiter";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Recruiters(ASR_RECRUITERS model)
+        {
+            if (ModelState.IsValid)
+            {
+                var db = new TransferredEntities1();
+                var recruiter = db.Submissions.FirstOrDefault(c => c.SLNO == 118).ASR_RECRUITERS;
+
+
+
+                //newPerson.Id = db.TestTables.Max(c => c.Id) + 1;
+                //newPerson.FirstName = model.FirstName;
+                //newPerson.LastName = model.LastName;
+                //db.TestTables.Add(newPerson);
+                //db.SaveChanges();
+                //ModelState.Clear();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Recruiters(int? val)
+        {
+            ViewBag.Message = val;
+            return View();
+        }
+
+        public ActionResult Consultants()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApp/ViewModels/TestTableViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.ViewModels
+{
+    public class TestTableViewModel
+    {
+        [Required(ErrorMessage = "Enter First Name")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Enter Last Name")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
